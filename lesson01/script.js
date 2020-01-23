94c7d534b174b364b05ee77653f9319c181717f7
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-01-23 22:01:05 +0200

    my first lesson

---
+++ lesson01/script.js
@@ -0,0 +1,11 @@
+let money = 100;
+let income = "hello";
+let addExpences = 25;
+let deposit = 97;
+let mission = "to do somthing";
+let period = [01, 02, 03];
+
+alert('Hello world!');
+
+console.log('Are you ready!');
+
