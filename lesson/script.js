4c83dd3d0997e3ef618a36b208243b118ca5b702
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-01-25 16:02:43 +0200

    task 2 fixed

--- lesson/script.js
+++ lesson/script.js
@@ -1,33 +1,19 @@
 let money = 1500;
-
-console.log(typeof(money));
-
 let income = "freelace";
-
-console.log(typeof(income));
-
 let addExpences = "Internet, Books, Car";
-
-console.log(addExpences.length);
-
+let deposit = true;
+let mission = 5000;
+let period = 12;  
+let budgetDay = money / 30;
 let arr = addExpences.toLowerCase().split(", ");
 
-console.log(arr);
-
-let deposit = true;
-
+console.log(typeof(money));
+console.log(typeof(income));
 console.log(typeof(deposit));
-
-let mission = 5000;
-
+console.log(addExpences.length);
 console.log("Цель заработать " + mission + " долларов");  
-
-let period = 12;   
-
 console.log("Период равен " + period + " месяцев");   
-
-let budgetDay = money / 30;
-
 console.log(budgetDay);
+console.log(arr);
 
 
