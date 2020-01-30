fd9a5739d3dde3dc0396597d694ef5417db99617
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-01-30 03:24:08 +0200

    task for lesson 5

--- lesson/script.js
+++ lesson/script.js
@@ -1,25 +1,55 @@
 "use strict";
 
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
 let income = "freelace",
     mission = 120000,
     period = 12,
-    money = +prompt("Ваш месячный доход?"),
+    money,
     addExpences = prompt("Перечислите возможные расходы за рассчитываемый период через запятую"),
     arr = addExpences.toLowerCase().split(", "),
     deposit = confirm("Есть ли у вас депозит в банке?"),
-    expenses1 = prompt("Введите обязательную статью расходов?"),
-    amount1 = +prompt("Во сколько это обойдется?"),
-    expenses2 = prompt("Введите обязательную статью расходов?"),
-    amount2 = +prompt("Во сколько это обойдется?");
+    expenses = [];
+
+let start = function () {
+
+    do {
+        money = prompt("Ваш месячный доход?");
+    }
+    
+    while (!isNumber(money));
+};
+
+start();
 
 let showTypeOf = function(data){
     return (typeof(data));
 };
 function getExpensesMonth(){
-    return amount1 + amount2;
+
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+        expenses[i] = prompt("Введите обязательную статью расходов?");
+        
+        let amount = prompt("Во сколько это обойдется?");
+
+        while (!isNumber(amount)) {
+            amount = prompt("Во сколько это обойдется?");
+        }
+
+        sum += +amount;
+    }
+
+    return sum;
 }
+
+let expensesAmount = getExpensesMonth();
+
 function getAccumulatedMonth(){
-    return money - getExpensesMonth();
+    return money - expensesAmount;
 }
 let accumulatedMonth = getAccumulatedMonth();
 function getTargetMonth(){
@@ -38,12 +68,14 @@ let getStatusIncome = function(){
     }
 };
 
+getTargetMonth() < 0 ? console.log("Цель не будет достигнута") : 
+console.log("Цель будет достигнута за " + getTargetMonth() + " месяцев");
+
 console.log(showTypeOf(money));
 console.log(showTypeOf(income));
 console.log(showTypeOf(deposit));
-console.log(getExpensesMonth());
+console.log(expensesAmount);
 console.log(arr);
-console.log("Цель будет достигнута за " + getTargetMonth() + " месяцев");
 console.log("Бюджет на день: " + budgetDay);
 console.log(getStatusIncome());
 
