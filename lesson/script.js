7702d29cf275f94775e10c347ebdb83ef9207aff
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-01-31 16:49:31 +0200

    lesson 06 hard task

--- lesson/script.js
+++ lesson/script.js
@@ -6,33 +6,62 @@ let isNumber = function(a) {
 
 function guess() {
 
-  let n = 88;
-  
-  function income() {
+    let n = 88;
+    let count = 10;
 
-      let start = prompt("Угадай число от 1 до 100");
+    function income() {
 
-      if (+start > n) {
-          alert("Загаданное число меньше");
-          income();
-      }
-      if (+start < n && start !== null) {
-          alert("Загаданное число больше");
-          income();
-      }
-      if (!isNumber(+start)) {
-          alert("Введи число!");
-          income();
-      }
-      if (start === null) {
-          return;
-      }
-      if (+start === n) {
-          return;
-      } 
-  }
-  
-  return income();   
+        let start = prompt("Угадай число от 1 до 100");
+
+        if (start === null) {
+            alert ("До скорой встречи!");
+            return;
+        }
+
+        start = start.trim();
+
+        if (!isNumber(+start) || start === "") {
+            alert("Введи число!");
+            return income();
+        }
+        
+        count--; 
+
+        if (+start === n) {
+            if (!confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+                alert ("До скорой встречи!");
+                return;
+            } else {
+                count = 10;
+                n = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+                return income();
+            } 
+        } 
+
+        if (count === 0){
+            if (!confirm("Попытки закончились, хотите сыграть еще?")) {
+                alert ("До скорой встречи!");
+                return;
+            } else {
+                count = 10;
+                return income();
+            }
+        }
+
+        if (+start > n) {
+            alert("Загаданное число меньше, у вас осталось " + count + " попыток");
+            return income();
+        }
+
+        if (+start < n && start !== null) {
+            alert("Загаданное число больше, у вас осталось " + count + " попыток");
+            return income();
+        }
+      
+    }
+
+    return income();
+    
 }
 
 guess();
