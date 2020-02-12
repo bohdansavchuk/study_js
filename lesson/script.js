8130ed33abef747f072d55c77473fc3ed068c9b9
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-02-13 01:24:56 +0200

    task first to lesson 14

--- lesson/script.js
+++ lesson/script.js
@@ -1,19 +1,34 @@
-let money = 1500;
-let income = "freelace";
-let addExpences = "Internet, Books, Car";
-let deposit = true;
-let mission = 5000;
-let period = 12;  
-let budgetDay = money / 30;
-let arr = addExpences.toLowerCase().split(", ");
+'use strict';
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
-console.log(addExpences.length);
-console.log("Цель заработать " + mission + " долларов");  
-console.log("Период равен " + period + " месяцев");   
-console.log(budgetDay);
-console.log(arr);
+function DomElement(selector, height, width, bg, fontSize){
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
 
+DomElement.prototype.create = function(){
+    let elem = this.selector;
+    let elemTag;
+
+    if(elem[0] === "."){
+        elemTag = document.createElement("div");
+        elemTag.classList.add(elem.slice(1));
+    } 
+    else if(elem[0] === "#") {
+        elemTag = document.createElement("p");
+        elemTag.id = elem.slice(1);
+    } else {
+        alert ("Введите верный селектор!");
+    }
+   
+    elemTag.textContent = "button";
+    elemTag.style.cssText = this.height + this.width + this.bg + this.fontSize;
+    document.body.prepend(elemTag);
+};
+
+let button = new DomElement(".btn", "height: 100px;", "width: 200px;", "background-color: red;", "font-size: 25px;");
+
+button.create();
 
