ffc89cca1e5d5492ebdb39c89ebc497c8cc043b8
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-02-13 12:20:44 +0200

    task hard to lesson 14

--- lesson/script.js
+++ lesson/script.js
@@ -1,34 +1,49 @@
-'use strict';
+"use strict";
 
-function DomElement(selector, height, width, bg, fontSize){
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-}
-
-DomElement.prototype.create = function(){
-    let elem = this.selector;
-    let elemTag;
-
-    if(elem[0] === "."){
-        elemTag = document.createElement("div");
-        elemTag.classList.add(elem.slice(1));
-    } 
-    else if(elem[0] === "#") {
-        elemTag = document.createElement("p");
-        elemTag.id = elem.slice(1);
-    } else {
-        alert ("Введите верный селектор!");
-    }
+document.addEventListener("DOMContentLoaded", function(event) {
    
-    elemTag.textContent = "button";
-    elemTag.style.cssText = this.height + this.width + this.bg + this.fontSize;
-    document.body.prepend(elemTag);
-};
+    function DomElement(selector, height, width, bg, position){
+        this.selector = selector;
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.position = position;
+    }
+    
+    DomElement.prototype.create = function(){
+        let elem = this.selector;
+        let elemTag;
+    
+        if(elem[0] === "."){
+            elemTag = document.createElement("div");
+            elemTag.classList.add(elem.slice(1));
+        } 
+        else if(elem[0] === "#") {
+            elemTag = document.createElement("p");
+            elemTag.id = elem.slice(1);
+        } else {
+            alert ("Введите верный селектор!");
+        }
+       
+        elemTag.style.cssText = this.height + this.width + this.bg + this.position;
+        document.body.prepend(elemTag);
 
-let button = new DomElement(".btn", "height: 100px;", "width: 200px;", "background-color: red;", "font-size: 25px;");
+        elemTag.style.left = 0;
+        elemTag.style.top = 0;
+        let positionX = 0;
+        let positionY = 0;
 
-button.create();
+        document.addEventListener("keydown", function(e){
+            if(e.keyCode === 39) return (elemTag.style.left = (positionX += 10) + "px");
+            if(e.keyCode === 37) return (elemTag.style.left = (positionX -= 10) + "px");
+            if(e.keyCode === 38) return (elemTag.style.top = (positionY -= 10) + "px");
+            if(e.keyCode === 40) return (elemTag.style.top = (positionY += 10) + "px");
+        });
+    };
+    
+    let button = new DomElement(".btn", "height: 100px;", "width: 100px;", "background-color: red;", "position: absolute;");
+    
+    button.create();
+
+});
 
